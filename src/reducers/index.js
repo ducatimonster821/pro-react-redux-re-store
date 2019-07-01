7946198e5d4bfa5862b18e012b3d15fd544defbf
vyacheslavsergin <vyacheslavsergin@gmail.com>
2019-07-01 20:51:17 +0500

    lesson134

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,31 @@
 const initialState = {
     books: [],
     loading: true,
-    error: null
+    error: null,
+    cartItems: [
+        {
+            id: 1,
+            name: 'Book 1',
+            count: 3,
+            total: 150
+        },
+        {
+            id: 2,
+            name: 'Book 2',
+            count: 2,
+            total: 70
+        }
+    ],
+    orderTotal: 220
 };
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'FETCH_BOOKS_REQUEST':
             return {
+                // cartItems: state.cartItems,
+                // orderTotal: state.orderTotal,
+                ...state,
                 books: [],
                 loading: true,
                 error: null
@@ -15,6 +33,7 @@ const reducer = (state = initialState, action) => {
 
         case 'FETCH_BOOKS_SUCCESS':
             return {
+                ...state,
                 books: action.payload,
                 loading: false,
                 error: null
@@ -22,6 +41,7 @@ const reducer = (state = initialState, action) => {
 
         case 'FETCH_BOOKS_FAILURE':
             return {
+                ...state,
                 books: [],
                 loading: false,
                 error: action.payload
